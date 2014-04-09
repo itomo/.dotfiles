e562e5d0b6df56ec35bb54b5e99acccb2e5e0b83
Tomohiro Inoue <tomohiro.inoue@gree.co.jp>
2014-04-09 10:31:16 +0900

    usage

--- sh/find_grep.sh
+++ sh/find_grep.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 WORD=$1
 TYPE=$2
+# 引数チェック
+if [ $# -ne 2 ]; then
+    echo "Usage: sh find_grep.sh <serch_word> <file_type>";
+    exit 1;
+fi
 echo "Run the following command:"
 echo "find . -name \"*.${TYPE}\" | xargs grep -n \`echo \"${WORD}\" | nkf -e \` | nkf -w | grep \"${WORD}\""
 echo ""
